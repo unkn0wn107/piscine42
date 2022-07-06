e1a7cd587f2dd4b6d80c562c184e837ab9f3a127
ukn0wn107 <unkn0wn@proton.me>
2022-07-06 15:15:47 +0200

    Enleve l escape pour sh exec sh01ex08

--- sh01/ex08/add_chelou.sh
+++ sh01/ex08/add_chelou.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "qbase=13;ibase=5;"$FT_NBR1+$FT_NBR2 | sed "y#'\\\"?\!#01234#" | sed "y#mrdoc#01234#" | sed 's/q/o/' | bc | sed "y#0123456789ABC#gtaio luSnemf#"
+echo "qbase=13;ibase=5;"$FT_NBR1+$FT_NBR2 | sed "y#'\\\"?!#01234#" | sed "y#mrdoc#01234#" | sed 's/q/o/' | bc | sed "y#0123456789ABC#gtaio luSnemf#"
