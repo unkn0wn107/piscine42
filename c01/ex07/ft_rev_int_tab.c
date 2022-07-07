eb089252357b3f4898ddd97b40e03b2cc7e2acea
ukn0wn107 <unkn0wn@proton.me>
2022-07-07 18:10:43 +0200

    Corrige c01ex07 pour respecter la norme

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,26 +6,32 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 17:10:54 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/07 17:57:22 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/07 18:09:58 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b)
+{
+	int	c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	rev_tab[size];
 	int	i;
-	int	j;
+	int	middle;
 
 	i = 0;
-	while (i < size)
+	if (size % 2)
+		middle = size / 2;
+	else
+		middle = size / 2 - 1;
+	while (i <= middle)
 	{
-		rev_tab[i] = tab[size - i - 1];
+		ft_swap(&tab[i], &tab[size - i - 1]);
 		i++;
-	}
-	j = 0;
-	while (j < size)
-	{
-		tab[j] = rev_tab[j];
-		j++;
 	}
 }
