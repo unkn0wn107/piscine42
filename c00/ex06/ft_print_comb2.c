c0cd122cccd64762b1978d7fe075b4b481225bea
ukn0wn107 <unkn0wn@proton.me>
2022-07-08 09:40:00 +0200

    Ajoute c00ex06

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:07:46 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/07 15:01:34 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/08 09:38:55 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putnum22(int i, int j, int k, int l, int withComma)
+void	ft_putnum22(int i, int j, int with_comma)
 {
 	char	a;
 	char	b;
 	char	c;
 	char	d;
 
-	a = i + '0';
-	b = j + '0';
-	c = k + '0';
-	d = l + '0';
+	a = i / 10 + '0';
+	b = i % 10 + '0';
+	c = j / 10 + '0';
+	d = j % 10 + '0';
 	ft_putchar(a);
 	ft_putchar(b);
 	ft_putchar(' ');
 	ft_putchar(c);
 	ft_putchar(d);
-	if (withComma)
+	if (with_comma)
 	{
 		ft_putchar(',');
 		ft_putchar(' ');
@@ -44,24 +44,17 @@ void	ft_print_comb2(void)
 {
 	int	i;
 	int	j;
-	int	k;
 
 	i = 0;
 	j = 0;
-	k = 0;
-	while (i <= 7)
+	while (i <= 98)
 	{
-		while (j <= 8)
+		while (j <= 99)
 		{
-			while (k <= 9)
-			{
-				if (k > j && j > i && (i != 7 || j != 8 || k != 9))
-					ft_putnum3(i, j, k, 1);
-				else if (k > j && j > i && i == 7 && j == 8 && k == 9)
-					ft_putnum3(i, j, k, 0);
-				k++;
-			}
-			k = 0;
+			if (j > i && (i != 98 || j != 99))
+				ft_putnum22(i, j, 1);
+			else if (j > i && i == 98 && j == 99)
+				ft_putnum22(i, j, 0);
 			j++;
 		}
 		j = 0;
--- c00/ex06/main.c
+++ c00/ex06/main.c
@@ -6,18 +6,23 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:19:42 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/07 15:01:37 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/08 09:38:57 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
 void ft_print_comb2();
-//void ft_putnum3(int i, int j, int k);
+void ft_putnum22(int i, int j, int with_comma);
 
 int main()
 {
+	int i = 33;
+	int j = 29;
+	char rn = '\n';
+	ft_putnum22(i, j, 1);
+	ft_putnum22(j, i, 0);
+	write(1, &rn, 1);
+	write(1, &rn, 1);
 	ft_print_comb2();
-	//int i = 3;
-	//int j = 2;
-	//int k = 8;
-	//ft_putnum3(i, j, k);
 	return 0;
 }
