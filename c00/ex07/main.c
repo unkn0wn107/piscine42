5774ce50bbdeec3f08bb0affc15c77a6c4cb695d
ukn0wn107 <unkn0wn@proton.me>
2022-07-08 10:46:38 +0200

    Ajoute c00ex07

---
+++ c00/ex07/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 18:19:42 by agaley            #+#    #+#             */
+/*   Updated: 2022/07/08 10:45:47 by agaley           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+void ft_putnbr(int nb);
+
+int main()
+{
+	char rn = '\n';
+	ft_putnbr(42);
+	write(1, &rn, 1);
+	ft_putnbr(INT_MAX);
+	write(1, &rn, 1);
+	ft_putnbr(INT_MIN);
+	//printf("%d", INT_MIN);
+	return 0;
+}
