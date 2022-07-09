98160ad81ae69986dabbfc0cd2966fe39a5bcba1
ukn0wn107 <unkn0wn@proton.me>
2022-07-09 12:13:29 +0200

    WIP c00ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 12:04:08 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/08 14:30:21 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/09 12:00:56 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,33 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putnbr(int nb)
+void	ft_putnbr_pos(unsigned int nb)
 {
 	if (0 <= nb && nb < 10)
 		ft_putchar(nb + '0');
-	else if (-2147483648 < nb && nb < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(-1 * nb);
-	}
-	else if (nb == -2147483648)
-	{
-		ft_putnbr(nb / 10);
-		ft_putchar(8 + '0');
-	}
 	else
 	{
-		ft_putnbr(nb / 10);
+		ft_putnbr_pos(nb / 10);
 		ft_putchar((nb % 10) + '0');
 	}
+}
+
+int	*ft_itodigits(*int nbr)
+{
+	int arr[9];
+	int i;
+	int m;
+
+	i = 0;
+	m = 1;
+	while(str[i] != '\0')
+		i++;
+	while(i >= 0)
+	{
+		arr[i] = nbr % m;
+		m *= 10;
+	}
+	return (arr);
 }
 
 //ft_putchar(',');
@@ -61,22 +69,22 @@ void	ft_print_combn(int n)
 	}
 }
 
-save void	ft_print_combn(int n)
-{
-	int	arr[8];
-
-	arr = {0,0,0,0,0,0,0,0,0};
-	while (i <= )
-	{
-		while (j <= 99)
-		{
-			if (j > i && (i != 98 || j != 99))
-				ft_putnum22(i, j, 1);
-			else if (j > i && i == 98 && j == 99)
-				ft_putnum22(i, j, 0);
-			j++;
-		}
-		j = 0;
-		i++;
-	}
-}
+//save void	ft_print_combn(int n)
+//{
+//	int	arr[8];
+//
+//	arr = {0,0,0,0,0,0,0,0,0};
+//	while (i <= )
+//	{
+//		while (j <= 99)
+//		{
+//			if (j > i && (i != 98 || j != 99))
+//				ft_putnum22(i, j, 1);
+//			else if (j > i && i == 98 && j == 99)
+//				ft_putnum22(i, j, 0);
+//			j++;
+//		}
+//		j = 0;
+//		i++;
+//	}
+//}
--- c00/ex08/main.c
+++ c00/ex08/main.c
@@ -6,23 +6,33 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:19:42 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/08 13:24:53 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/09 12:00:59 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <unistd.h>
-void ft_print_combn();
-void ft_putnumn(int i, int n, int with_comma);
+void	ft_putnbr_pos(unsigned int nb);
+int		*ft_itodigits(*int nbr);
+void	ft_print_combn(int n);
+//void	ft_putnumn(int i, int n, int with_comma);
 
 int main()
 {
-	int i = 3305;
+	const char RN = '\n';
+	int i = 0;
 	int j = 2998516;
-	char rn = '\n';
-	ft_putnum22(i, j, 1);
-	ft_putnum22(j, i, 0);
-	write(1, &rn, 1);
-	write(1, &rn, 1);
-	ft_print_combn();
+	int arr[9];
+	ft_putnbr_pos(j);
+	write(1, &RN, 1);
+	arr = ft_itodigits(j);
+	while(i < 9)
+	{
+		write(1, arr[i] + '0', 1);
+		i++;
+	}
+	//ft_putnum22(i, j, 1);
+	//ft_putnum22(j, i, 0);
+	write(1, &RN, 1);
+	//ft_print_combn();
 	return 0;
 }
