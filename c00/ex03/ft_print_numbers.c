19d28f22d7cc5a65421432c1e5a25c4458d80957
ukn0wn107 <unkn0wn@proton.me>
2022-07-12 13:41:26 +0200

    Nettoie c00ex03

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:07:46 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/06 19:59:06 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/12 10:39:11 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ void	ft_putchar(char c)
 void	ft_print_numbers(void)
 {
 	int		i;
-	int		n;
 	char	c;
 
 	i = 0;
-	n = 0;
 	while (i < 10)
 	{
 		c = i + '0';
 		ft_putchar(c);
-		n--;
 		i++;
 	}
 }
