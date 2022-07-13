d3303a24678827b5600f2bf0ecb8b231fd9fa772
ukn0wn107 <unkn0wn@proton.me>
2022-07-13 16:34:57 +0200

    Ajoute c02 rendu 2 et 3

---
+++ c02/c02_rendu_2/ex10/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 23:36:06 by agaley            #+#    #+#             */
+/*   Updated: 2022/07/13 14:04:19 by agaley           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dest[i] = '\0';
+	return (ft_strlen(src));
+}
---
+++ c02/c02_rendu_3/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 00:11:02 by agaley            #+#    #+#             */
+/*   Updated: 2022/07/13 16:17:38 by agaley           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar_non_printable(char c, int with_zero)
+{
+	char	*base;
+	char	d;
+
+	base = "0123456789abcdef";
+	if (c < 16)
+	{
+		d = base[(int)c];
+		write(1, "\\", 1);
+		if (with_zero)
+			write(1, "0", 1);
+		write(1, &d, 1);
+	}
+	else
+	{
+		ft_putchar_non_printable(c / 16, 0);
+		d = base[c % 16];
+		write(1, &d, 1);
+	}
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		c = str[i];
+		if (c < 32 || c > 126)
+			ft_putchar_non_printable(c, 1);
+		else
+			write(1, &c, 1);
+		i++;
+	}
+}
