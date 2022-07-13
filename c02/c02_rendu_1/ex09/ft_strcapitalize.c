518f71a0cbea9cea822b5e4bccb954dd9b52e98d
ukn0wn107 <unkn0wn@proton.me>
2022-07-13 14:20:13 +0200

    Ajoute c02 rendu 1

---
+++ c02/c02_rendu_1/ex09/ft_strcapitalize.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 22:56:17 by agaley            #+#    #+#             */
+/*   Updated: 2022/07/12 11:50:57 by agaley           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		c = str[i];
+		if ('A' <= c && c <= 'Z')
+			str[i] = c + 'a' - 'A';
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	char	c;
+	char	d;
+
+	ft_strlowcase(str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		c = str[i];
+		d = str[i + 1];
+		if (i == 0 && 'a' <= c && c <= 'z')
+			str[0] = c - 'a' + 'A';
+		if ('a' <= d && d <= 'z' && (c < '1' || c > 'z'
+				|| (90 < c && c < 97)
+				|| (57 < c && c < 65)))
+			str[i + 1] = d - 'a' + 'A';
+		i++;
+	}
+	return (str);
+}
---
+++ c02/c02_rendu_1/ex10/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 20:23:43 by agaley            #+#    #+#             */
+/*   Updated: 2022/07/12 00:08:56 by agaley           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+
+int	main(void)
+{
+	int	i = 0;
+	int len = 0;
+	char src[][35] = {"ABC", "Null-terminated\0", "Plus que la taille de destination", "/"};
+	char dest[20];
+	while (i < 4)
+	{
+		len = ft_strlcpy(dest, src[i], 20);
+		printf("%s => %s // len %d\n", src[i], dest, len);
+		i++;
+	}	
+	i = 0;
+	printf("Vraie strlcpy :\n");
+	while (i < 4)
+	{
+		len = strlcpy(dest, src[i], 20);
+		printf("%s => %s // len %d\n", src[i], dest, len);
+		i++;
+	}	
+}
