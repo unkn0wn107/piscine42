8f25b98cc51daf505af822bfb5fab6356861ce7a
ukn0wn107 <unkn0wn@proton.me>
2022-07-14 14:01:01 +0200

    Corrige c03

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: agaley <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 14:48:07 by agaley            #+#    #+#             */
-/*   Updated: 2022/07/12 14:53:27 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2022/07/13 21:51:37 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && s1[i] && s2 && s1[i] == s2[i])
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
 }
